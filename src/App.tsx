11a2e7971c042699731b27b8f319c45b623889c9
Artur Yakhin <artur.yakhin52@gmail.com>
2022-10-31 16:19:33 +0500

    header desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Header from './components/Header'
 import './styles/app.scss'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,16 @@ import { FC } from 'react'
 import styles from './Header.module.scss'
 
 const Header: FC = () => {
-  return <div className={styles.Header}></div>
+  return (
+    <div className={styles.Header}>
+      <div className="container">
+        <div className={styles.inner}>
+          <h1>Posop Dev</h1>
+          <span className={styles.lang}>EN</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
