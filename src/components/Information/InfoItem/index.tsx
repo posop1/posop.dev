56d383ca68dfb8ea5c27f004294b98c844c79c87
Artur Yakhin <artur.yakhin52@gmail.com>
2022-10-31 18:34:40 +0500

    add information component item

---
+++ src/components/Information/InfoItem/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import { IText } from '../../../types'
+import styles from './InfoItem.module.scss'
+
+const InfoItem: FC<IText> = ({ title, body, stack }) => {
+  return (
+    <div className={styles.InfoItem}>
+      <div>
+        <span>
+          {title},{body}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default InfoItem
