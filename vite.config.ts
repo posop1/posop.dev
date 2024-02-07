796ac00d1fa1f1764cf8474cf331671ce844610a
posop <artur.yakhin52@gmail.com>
2024-02-07 19:20:56 +0500

    feat: add vite-plugin-circular-dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import circleDependency from 'vite-plugin-circular-dependency'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    circleDependency({
+      outputFilePath: './circleDep'
+    })
+  ],
   server: {
     watch: {
       usePolling: true
